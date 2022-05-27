6dd0ad4c2ae99b7d1dfa9bb4fb3904a005854173
KanishkRanjan <68316017+KanishkRanjan@users.noreply.github.com>
2022-05-27 13:24:48 +0530

    Create snippets.cpp

---
+++ snippets.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++h.>
+
+using namespace std;
+
+#define vi vector<int> 
+typedef pair<int, int> pii;
+typedef pair<int, char> pic;
+
+#define LL long long int
+#define ST string
+#define ed << '\n';
+#define pl(s) cout<< s ed
+#define F first
+#define S second
+#define MP make_pair
+#define PB push_back
+#define POB pop_back
+
+#define ALL(v) v.begin(),v.end()
+#define ALLA(arr, sz) arr, arr + sz
+
+#define FOR(a,c) for(int (a) = 0; (a) < (c); (a)++)
+
+#define SORT(v) sort(ALL(v))
+#define SORTA(arr,asize) sort(ALLA(arr,asize));
+
+
+int main()
+{
+
+
+    return 0;
+}
+
